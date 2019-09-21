5d0adf6a9bf83dcc763a3e6cad2cd0efb4bbd58d
Christoph <christoph@ag-prop.com>
2019-09-21 21:42:59 +0200

    Feat(Debug): Add effect debug mode

--- app/components/video.js
+++ app/components/video.js
@@ -3,9 +3,10 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default class ContentVideoComponent extends Component {
+export default class VideoComponent extends Component {
   // Services
   @service media;
+  @service debug;
 
 
   // Defaults
--- app/services/debug.js
+++ app/services/debug.js
@@ -2,21 +2,29 @@ import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class DebugService extends Service {
+  // Defaults
   @tracked infoEnabled = true;
   @tracked videoEnabled = false;
+  @tracked effectEnabled = true;
 
+  // Hooks
   constructor() {
     super(...arguments);
 
     document.addEventListener('keypress', this._enableDebug.bind(this));
   }
 
+
+  // Functions
   _enableDebug(e) {
     if(e.key == 'i') {
       this.infoEnabled = !this.infoEnabled;
     }
     if(e.key == 'v') {
       this.videoEnabled = !this.videoEnabled;
+    }
+    if(e.key == 'e') {
+      this.effectEnabled = !this.effectEnabled;
     }
   }
 }
